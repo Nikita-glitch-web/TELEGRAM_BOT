5d07d668290570c57a6133b2de33eecabef77345
Nick <nikitalytvynov0506@gmail.com>
2023-08-18 20:14:41 +0300

    sii

--- controllers/weather-logic.js
+++ controllers/weather-logic.js
@@ -3,7 +3,7 @@ const fetch = require("node-fetch");
 
 const API_URL = `https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={06c5f199747cce8b744a02e23013298f}`;
 
-const weekUrl = `https://api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={06c5f199747cce8b744a02e23013298f}`;
+const weekUrl = `https://api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={cnt}&appid={06c5f199747cce8b744a02e23013298f}`;
 
 // нова урл на 16 днив
 
@@ -11,8 +11,11 @@ const createRequestCityUrl = (cityName) => {
   return `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=06c5f199747cce8b744a02e23013298f`;
 };
 
+//   "lon": 30.5167,
+//         "lat": 50.4333
+
 const createWeekRequestUrl = (lat, lon) => {
-    return `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&cnt={06c5f199747cce8b744a02e23013298f}`
+    return `api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&cnt={1}&appid={06c5f199747cce8b744a02e23013298f}`;
 }
 
 
@@ -31,22 +34,31 @@ exports.requsetToServer = async (req, res, next) => {
   next();
 };
 
-exports.weekWeatherRes = async (req, res, next) => {
+exports.cityCoordinateRes = async (req, res, next) => {
     const city = req.params.city;
-    const weekResponse = await fetch(createRequestCityUrl(city));
-    const weekData = await weekResponse.json();
+    const cityResponse = await fetch(createRequestCityUrl(city));
+    const cityData = await cityResponse.json();
     const cityMappedData = {
-        lat: `${weekData.coord.lat}`,
-        lon: `${weekData.coord.lon}`,
-        cityName: `${weekData.name}`
-    }
-
+        lat: `${cityData.coord.lat}`,
+        lon: `${cityData.coord.lon}`,
+        cityName: `${cityData.name}`
+}
     // const weekMappedData = {
     //     date: `${weekData.date}`
     // }
     res.send(cityMappedData);
-    console.log(cityMappedData)
+    console.log(cityMappedData);
+    next();
 }
+
+exports.weekRes = async (req, res, next) => {
+    const week = req.params.week
+    const weekResponse = await fetch(createWeekRequestUrl(week));
+    const weekData = await cityResponse.json();
+    const weekMappedData = {
+
+    }
+};
 
 
 // Параметры запроса URL (URL Query Parameters) — это 
